fa7124d34def9d5b935c841d25023405c2b99194
Roy <royroy.euph@gmail.com>
2020-01-17 13:12:16 +0900

    リポジトリ登録のテストを追加

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -6,13 +6,16 @@ import tempfile
 from tzlocal import get_localzone
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + os.sep + os.pardir))
-import edit
+import edit_db
 import entry_point
+
+
+TEST_DB_FILE = 'testdb.sqlite'
 
 
 def test_create_table():
     with tempfile.TemporaryDirectory() as tempdir:
-        entry_point.DB_FILE = tempdir + os.sep + "testdb.sqlite"
+        entry_point.DB_FILE = tempdir + os.sep + TEST_DB_FILE
 
         if os.path.exists(entry_point.DB_FILE):
             os.remove(entry_point.DB_FILE)
@@ -39,30 +42,30 @@ def test_create_table():
             row1['last_updated'] = None
             row2['publisher'] = 'library'
             row2['last_updated'] = None
-            data = db['watching_repositories'].find(order_by=db['watching_repositories'].columns)
+            results = db['watching_repositories'].find(order_by=db['watching_repositories'].columns)
 
-            assert data.next() == row1
-            assert data.next() == row2
+            assert results.next() == row1
+            assert results.next() == row2
 
-            data.close()
+            results.close()
             db.close()
-        del data
+        del results
         del db
         gc.collect()
 
 def test_add_user():
     with tempfile.TemporaryDirectory() as tempdir:
-        entry_point.DB_FILE = tempdir + os.sep + "testdb.sqlite"
+        entry_point.DB_FILE = tempdir + os.sep + TEST_DB_FILE
 
         if os.path.exists(entry_point.DB_FILE):
             os.remove(entry_point.DB_FILE)
 
         entry_point.db_create()
 
-        edit.register_user('test_user1', 'test@gmail.com')
-        edit.register_user('test_user2', 'http://hooks.slack.com/services/test')
-        edit.register_user('test_user3', 'test@test.test')
-        edit.register_user('test_user4', 'http://test.test/services/test')
+        edit_db.register_user('test_user1', 'test@gmail.com')
+        edit_db.register_user('test_user2', 'http://hooks.slack.com/services/test')
+        edit_db.register_user('test_user3', 'test@test.test')
+        edit_db.register_user('test_user4', 'http://test.test/services/test')
 
         with entry_point.get_db() as db:
             assert db['users'].count() == 2
@@ -70,6 +73,107 @@ def test_add_user():
             assert db['users'].find_one(user_id='test_user1')['webhook_url'] == None
             assert db['users'].find_one(user_id='test_user2')['mail_address'] == None
             assert db['users'].find_one(user_id='test_user2')['webhook_url'] == 'http://hooks.slack.com/services/test'
+
+            db.close()
+        del db
+        gc.collect()
+
+
+def test_remove_user():
+    with tempfile.TemporaryDirectory() as tempdir:
+        entry_point.DB_FILE = tempdir + os.sep + TEST_DB_FILE
+
+        if os.path.exists(entry_point.DB_FILE):
+            os.remove(entry_point.DB_FILE)
+
+        entry_point.db_create()
+
+        edit_db.register_user('test_user1', 'test@gmail.com')
+        edit_db.register_user('test_user2', 'http://hooks.slack.com/services/test')
+
+        with entry_point.get_db() as db:
+            assert db['users'].count() == 2
+
+            edit_db.delete_user('test_user1')
+
+            assert db['users'].count() == 1
+
+            edit_db.delete_user('test_user3')
+
+            assert db['users'].find_one(user_id='test_user1') is None
+            assert db['users'].find_one(user_id='test_user2') is not None
+            assert db['users'].find_one(user_id='test_user3') is None
+
+            db.close()
+        del db
+        gc.collect()
+
+
+def test_add_repo():
+    with tempfile.TemporaryDirectory() as tempdir:
+        entry_point.DB_FILE = tempdir + os.sep + TEST_DB_FILE
+
+        if os.path.exists(entry_point.DB_FILE):
+            os.remove(entry_point.DB_FILE)
+
+        entry_point.db_create()
+
+        edit_db.register_user('test_user1', 'test@gmail.com')
+
+        with entry_point.get_db() as db:
+            keys1 = dict(user_seq=1, publisher='library', repo_name='alpine', repo_tag='latest')
+            check1 = dict(keys1.items())
+            check1['last_updated'] = None
+            edit_db.subscribe_repository('test_user1', 'alpine')
+            assert db['watching_repositories'].count() == 1
+            result1 = db['watching_repositories'].find_one(**keys1)
+            assert result1['last_updated'] is not None
+            result1['last_updated'] = None
+            assert result1 == check1
+
+            edit_db.subscribe_repository('test_user1', 'alpine') # duplicate.
+            assert db['watching_repositories'].count() == 1
+
+            edit_db.subscribe_repository('test_user1', 'alpine:latest') # duplicate.
+            assert db['watching_repositories'].count() == 1
+            result1 = db['watching_repositories'].find_one(**keys1)
+            result1['last_updated'] = None
+            assert result1 == check1
+
+
+            edit_db.subscribe_repository('test_user1', 'alpine:3.10') # not duplicate.
+            assert db['watching_repositories'].count() == 2
+            keys2 = dict(keys1.items())
+            keys2['repo_tag'] = '3.10'
+            check2 = dict(keys2.items())
+            check2['last_updated'] = None
+
+            result1 = db['watching_repositories'].find_one(**keys1)
+            result1['last_updated'] = None
+            assert result1 == check1
+            result2 = db['watching_repositories'].find_one(**keys2)
+            result2['last_updated'] = None
+            assert result2 == check2
+
+            edit_db.subscribe_repository('test_user1', 'alpine:1') # not exists.
+            assert db['watching_repositories'].count() == 2
+
+            db.close()
+        del db
+        gc.collect()
+
+
+def test_remove_repo():
+    with tempfile.TemporaryDirectory() as tempdir:
+        entry_point.DB_FILE = tempdir + os.sep + TEST_DB_FILE
+
+        if os.path.exists(entry_point.DB_FILE):
+            os.remove(entry_point.DB_FILE)
+
+        entry_point.db_create()
+
+
+        with entry_point.get_db() as db:
 
             db.close()
         del db
