6b087443970ec09de131c3bcb7906e988613bec2
Roy <royroy.euph@gmail.com>
2020-01-22 00:09:18 +0900

    フォルダ名変更、Docker用ファイル整備

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,12 @@
 FROM python:3.8
 
-ENV SMTP_HOST smtp.gmail.com
-ENV SMPT_PORT 465
+ENV PYTHONUNBUFFERED 1
 
-WORKDIR /data
 WORKDIR /app
 
-COPY requirements.txt .
+COPY django/ /app/
 
-RUN curl -L https://github.com/pudo/dataset/archive/1.2.0.tar.gz -o dataset-1.2.0.tar.gz && tar xf dataset-1.2.0.tar.gz && pip install -r requirements.txt ./dataset-1.2.0
+RUN pip install -r requirements.txt
 
-ENV PATH /app:$PATH:
-
-COPY *.py /app/
-RUN cat edit_db.py | tr -d '\r' > edit_db; chmod +x edit_db
-
-CMD [ "python3", "./entry_point.py" ]
+EXPOSE 3031
+CMD [ "uwsgi", "./uwsgi.ini" ]
---
+++ django/config/settings_dev.py
@@ -0,0 +1,25 @@
+import os
+
+from .settings import *
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '3z&wej7%6pm8m66s#g@o&n*k2ow-r+-%jpzrg^#==bnl7y#89g'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
