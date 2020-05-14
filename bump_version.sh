2e604765fa83447df136fd4a9d1e6bda5aef0d52
Roy <royroy.euph@gmail.com>
2020-05-14 13:53:56 +0900

    バージョン更新スクリプトを追加

---
+++ bump_version.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $# != 1 ]]; then
+	exit 1
+fi
+
+VERSION=$1
+
+if [[ $(git branch --show-current) != "master" ]]; then
+	echo "It's not a master branch."
+	exit 1
+fi
+
+if [[ ! ${VERSION} =~ ^v[0-9]+\.[0-9]+\.[0-9]+[ab]?$ ]]; then
+	echo "The version number is invalid."
+	echo "E.g. v1.2.3"
+	exit 1
+fi
+
+poetry version ${VERSION}
+
+echo "__version__ = '${VERSION}'" > ./django/config/__init__.py
+
+git add pyproject.toml ./django/config/__init__.py
+
+git commit -m "Bump version to ${VERSION}"
+
+git log -1 ${VERSION} && git tag -d ${VERSION}
+git ls-remote --exit-code origin ${VERSION} && git push -d origin ${VERSION}
+
+git tag ${VERSION}
+git push origin master ${VERSION}
